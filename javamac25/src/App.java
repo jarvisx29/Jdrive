fa9091bed723f709c8a86126f268470d342a59e4
G mano prasanna <gmanoprasanna@gmail.com>
2025-09-24 23:06:49 +0530

    sept 24th night

--- javamac25/src/App.java
+++ javamac25/src/App.java
@@ -7391,7 +7391,19 @@ class Shortest_Palindrome   // 100 % working in leetcode (just last 2 testcases 
 }
 
 
-class longest_subarray_length 
+class longest_subarray_length     //  Problem Statement: Given an array and a sum k, we need to print the length of the longest subarray that sums to k.  Examples
+
+// Example 1:
+// Input Format: N = 3, k = 5, array[] = {2,3,5}
+// Result: 2
+// Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+// Example 2:
+// Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+// Result: 3
+// Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+
 {
     public static void main(String[] args) 
     {
@@ -7399,30 +7411,68 @@ class longest_subarray_length
 
         int k = 10;
         int sum =0;
-        int count =2;
+        int count =0;
+        int num =2;
 
         for(int i=0;i<arr.length;i++)
         {
             for(int j =i+1;j<arr.length;j++)
             {
-                sum = sum + arr[i]+ arr[j];
-                count++;
+                sum = sum + arr[i] + arr[j];
+                
+                // int arr2 [] = new int [arr.length];
+                // arr2 = arr[i]+arr[j];
+                
                 
                 if(sum == k)
                 {
+                    count++;
                     break;
-
                 }
                 
             }
+            count++;
         }
 
-        System.out.println("the length of the longest subarray is : "+count);
+        System.out.println("the length of the longest subarray is : "+(count-num));
 
         
     }
 
 }
+
+class Majority_Element
+{
+    public static void main(String[] args) 
+    {
+        int arr [] = {2,2,1,1,1,2,2};
+
+        int occurence = arr.length/2;
+        int count =0;
+
+        int max_count_element=0;
+        
+        for(int i =0;i<arr.length;i++)
+        {
+            for(int j =i+1;j<arr.length;j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
+
+            if(count > occurence)
+            {
+                max_count_element = arr[i];
+                break;
+            }
+        }
+
+        System.out.println("the the element that appears more than n/2 times : "+max_count_element);
+    }
+}
+
 
 
 
