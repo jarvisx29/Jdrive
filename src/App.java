4d72c320b4c2895b3412849278e6f19f8c96c6ba
G mano prasanna <gmanoprasanna@gmail.com>
2025-10-16 23:46:13 +0530

    oct 16th night

--- src/App.java
+++ src/App.java
@@ -8270,6 +8270,39 @@ class Count_Increasing_Quadruplets   // leetcode quwstion (time limit exceeded)
 
 
 
+class Increasing_Triplet_Subsequence
+{
+    public static boolean main(String[] args) 
+    {
+        int arr [] ={1,2,3,4,5};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                for(int k= 0 ;k<arr.length;k++)
+                {
+                    if(arr[i]<arr[j] && arr[j] <arr[k])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+
+
+
+
+
+
 
 
 
