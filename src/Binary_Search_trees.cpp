2a69235f017a890de7d2d34a21f31e8827f54543
Mano Prasanna <manoprasanna@Manos-MacBook-Pro.local>
2026-03-23 03:40:47 +0530

    mar 23rd night binary search trees completed yayyyy

--- src/Binary_Search_trees.cpp
+++ src/Binary_Search_trees.cpp
@@ -412,7 +412,19 @@ int main()
 
 
 
-// POSTORDER DISPLAY USING 2 STACKS (stack <node *> st1,st2)
+
+
+
+
+
+
+
+
+// POSTORDER DISPLAY USING 2 STACKS (stack <node *> st1,st2) // senthil sir gave wrong; review and paste correct pics here !!!!1
+
+
+
+
 
 #include <iostream>
 #include <stack>.     //         paste page pics immediately !!!!!!!!1!!!!     
@@ -550,6 +562,277 @@ int main()
     
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// INORDER TRAVESAL     (using vector,stack (1vec,1stack))    // 2026-03-23-03-24-46.png
+
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left = nullptr;
+    node *right = nullptr;
+};
+
+void where_to_place(node *r,node *newnode)
+{
+    node *prev =nullptr;
+    
+    bool left_flag = false;
+    bool right_flag = false;
+    
+    while(r != nullptr)
+    {
+        left_flag = false;
+        right_flag = false;
+        
+        if(newnode->data < r->data)
+        {
+            prev = r;
+            r = r->left;
+            left_flag = true;
+        }
+        else
+        {
+            prev =r;
+            r = r->right;
+            right_flag = true;
+        }
+    }
+    
+    if(left_flag ==true)
+    {
+        prev->left = newnode;
+    }
+    if(right_flag == true)
+    {
+        prev->right = newnode;
+    }
+    
+}
+
+void inOrder(node *r,vector <int> &vec)
+{
+    stack <node *> st;
+    node *cur = r;
+    
+    while(cur !=nullptr || st.empty() == false)
+    {
+        while(cur != nullptr)
+        {
+            st.push(cur);
+            cur = cur->left;
+        }
+        
+        cur = st.top();
+        st.pop();
+        
+        vec.push_back(cur->data);
+        cur = cur->right;
+    }
+    
+    
+    
+}
+
+void print_vector(vector <int> &vec)
+{
+    for(int i=0;i<vec.size();i++)
+    {
+        printf(" %d",vec[i]);
+    }
+}
+
+
+
+int main()
+{
+    node *root =  new node();
+    root->data = 50;
+    
+    node * newnode_1 = new node();
+    newnode_1->data = 30;
+    
+    node *newnode_2 = new node();
+    newnode_2->data = 20;
+    
+    node *newnode_3 = new node();
+    newnode_3->data = 40;
+    
+    node *newnode_4 = new node();
+    newnode_4->data = 70;
+    
+    node *newnode_5 = new node();
+    newnode_5->data = 60;
+    
+    node *newnode_6 = new node();
+    newnode_6->data = 80;
+    
+
+    
+    where_to_place(root,newnode_1);
+    where_to_place(root,newnode_2);
+    where_to_place(root,newnode_3);
+    where_to_place(root,newnode_4);
+    where_to_place(root,newnode_5);
+    where_to_place(root,newnode_6);
+    
+    vector <int> vec;
+    
+    inOrder(root,vec);
+    
+    print_vector(vec);
+}
+
+
+
+
+
+
+
+
+
+
+
+// LEVEL ORDER TRAVERSAL  (using 1 queue thats it ! ) //.    2026-03-23-03-25-36.png
+
+
+
+#include <iostream>
+#include <stack>
+#include <vector>
+#include<queue>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left = nullptr;
+    node *right = nullptr;
+};
+
+void where_to_place(node *r,node *newnode)
+{
+    node *prev = nullptr;
+    
+    bool left_flag = false;
+    bool right_flag = false;
+    
+    while(r != nullptr)
+    {
+        left_flag = false;
+        right_flag = false;
+        
+        if(newnode->data < r->data)
+        {
+            prev = r;
+            r = r->left;
+            left_flag = true;
+        }
+        else
+        {
+            prev =r;
+            r = r->right;
+            right_flag = true;
+        }
+    }
+    
+    if(left_flag ==true)
+    {
+        prev->left = newnode;
+    }
+    if(right_flag == true)
+    {
+        prev->right = newnode;
+    }
+}
+
+
+void level_Order(node *r)
+{
+    if(r ==nullptr)
+    {
+        return;
+    }
+    
+    queue<node *> q;
+    q.push(r);
+    
+    while(!q.empty())
+    {
+        node *cur = q.front();
+        q.pop();
+        
+        printf(" %d",cur->data);
+        
+        if(cur->left != nullptr)
+        {
+            q.push(cur->left);
+        }
+        if(cur->right != nullptr)
+        {
+            q.push(cur->right);
+        }
+        
+    }
+    
+}
+
+
+int main()
+{
+    node *root =  new node();
+    root->data = 50;
+    
+    node * newnode_1 = new node();
+    newnode_1->data = 30;
+    
+    node *newnode_2 = new node();
+    newnode_2->data = 20;
+    
+    node *newnode_3 = new node();
+    newnode_3->data = 40;
+    
+    node *newnode_4 = new node();
+    newnode_4->data = 70;
+    
+    node *newnode_5 = new node();
+    newnode_5->data = 60;
+    
+    node *newnode_6 = new node();
+    newnode_6->data = 80;
+    
+
+    
+    where_to_place(root,newnode_1);
+    where_to_place(root,newnode_2);
+    where_to_place(root,newnode_3);
+    where_to_place(root,newnode_4);
+    where_to_place(root,newnode_5);
+    where_to_place(root,newnode_6);
+    
+    level_Order(root);
+}
 
 
 
